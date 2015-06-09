7c5b23bcb95ad701627c3f39d20cd69ce9df08ce
Erik Jenks <erikjenks@gmail.com>
2015-06-09 09:48:57 -0400

    fixed views and controller

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -3,4 +3,8 @@ class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
   end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
 end
