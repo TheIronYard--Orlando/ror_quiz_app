5ee3ae18a141057b59e9264f7645017dac637a2d
Heidi Roude <HeidiRoude@gmail.com>
2015-06-11 14:29:14 -0400

    updating 0610

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -5,20 +5,26 @@ class ManufacturersController < ApplicationController
   end
 
   def show
-  	@manufacturer = Manufacturer.find(params[:id])
+    @manufacturer = Manufacturer.find(params[:id])
   end
 
   def edit
-  	@manufacturer = Manufacturer.find(params[:id])
+    @manufacturer = Manufacturer.find(params[:id])
   end
 
   def update
-  	@manufacturer = Manufacturer.find(params[:id])
-  	@manufacturer.update(params.require(:manufacturer),permit(:name, :domestic))
-  	flash[:notice] = "Manufacturer successfully updated!"
-  	redirect_to manufacturer_url
+    @manufacturer = Manufacturer.find(params[:id])
+    @manufacturer.update(params.require(:manufacturer).permit(:name, :domestic))
+    flash[:notice] = "Manufacturer successfully updated!"
+    redirect_to manufacturer_url
   end
-  	
+
+  def destroy
+    @manufacturer = Manufacturer.find(params[:id])
+    @manufacturer.destroy
+    redirect_to root_url
+  end
+
 end
 
 
