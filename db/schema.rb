67ff28713692b3bd9516c0d930b88b19df146679
Mark Sadler <msadler10@hotmail.com>
2015-06-15 12:02:45 -0400

    add new database for car models and relate to manufacturer database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609133522) do
+ActiveRecord::Schema.define(version: 20150615155724) do
+
+  create_table "car_models", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "mpg"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "manufacturers", force: :cascade do |t|
     t.string   "name"
