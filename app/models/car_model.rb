33452b9c07262b91cf48c0edce2bb5f86dc5aa9e
Erik Jenks <erikjenks@gmail.com>
2015-06-24 09:46:29 -0400

    added migration and table

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,6 +1,7 @@
 class CarModel < ActiveRecord::Base
     belongs_to :manufacturer
-
+    has_many :production_lines
+    has_many :factories through: :production_lines
     has_and_belongs_to_many :factories
     validates :name, presence: true, uniqueness: true
 
--- db/migrate/20150624130020_promote_join_table_to_model.rb
+++ db/migrate/20150624130020_promote_join_table_to_model.rb
@@ -1,4 +1,7 @@
 class PromoteJoinTableToModel < ActiveRecord::Migration
   def change
+    rename_table :car_models_factories, :production_lines do |t|
+      t.integer :annual_quota
+    end
   end
 end
