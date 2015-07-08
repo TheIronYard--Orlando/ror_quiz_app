0651bcae49a233f020245e82fb6fc6fe193f899d
Erik Jenks <erikjenks@gmail.com>
2015-07-08 09:25:20 -0400

    fixed

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,20 +1,18 @@
 class ManufacturersController < ApplicationController
   before_action :require_login, only: [:edit, :update, :destroy]
+  before_action :manufacturer_id, except: [:index]
 
   def index
     @manufacturers = Manufacturer.all
   end
 
   def show
-    manufacturer_id
   end
 
   def edit
-    manufacturer_id
   end
 
   def update
-    manufacturer_id
     @manufacturer.update(params.require(:manufacturer).permit(:name, :domestic))
     flash[:notice] = "Manufacturer successfully updated!"
     redirect_to manufacturer_url
